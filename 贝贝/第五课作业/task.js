da00a5d4cdd8b10550dfce2749d286b0f11d5a25
bass <839720195@qq.com>
2020-12-26 08:38:06 +0800

    修改第五课作业

--- 贝贝/第五课作业/task.js
+++ 贝贝/第五课作业/task.js
@@ -4,9 +4,12 @@ const fs = require('fs');
 // 获取当前文件所在的目录
 console.log(__dirname)
 console.log(path.dirname(__filename))
+console.log(path.resolve())
 
 // 获取当前文件的文件名
 console.log(path.basename(__filename))  // task.js
+console.log(path.parse(__filename).base)  // task.js
 
 // 获取当前文件的扩展名
 console.log(path.extname(__filename))  // .js
+console.log(path.parse(__filename).ext)  // .js
